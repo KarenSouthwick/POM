fa86c0c3dbfb02dc55a2e6765cf311c6f44152e7
KarenSouthwick <Karen.Southwick@Authenticateis.com>
2018-07-04 12:14:33 +0100

    added another test, close prod tour

--- POM/Tests/Click.cs
+++ POM/Tests/Click.cs
@@ -9,6 +9,7 @@ using OpenQA.Selenium.Support.PageObjects;
 using NUnit.Framework;
 using POM.PageObjects;
 using SeleniumExtras.PageObjects;
+using System.Threading;
 
 namespace POM.Tests
 {
@@ -42,6 +43,22 @@ namespace POM.Tests
             PopUp popup = new PopUp(driver);
             SeleniumExtras.PageObjects.PageFactory.InitElements(driver, popup);
             popup.ClosePopUp();
+
+            Thread.Sleep(3000);
+            driver.FindElement(By.LinkText("My Products")).Click();
+
+            PopUp popup2 = new PopUp(driver);
+            SeleniumExtras.PageObjects.PageFactory.InitElements(driver, popup2);
+            popup2.ClosePopUp();
+
+            Thread.Sleep(3000);
+            driver.FindElement(By.XPath("//div[@id='do-categoryRootNav']/div/div/ul/li[3]/div/div[2]/p/b")).Click();
+            driver.FindElement(By.XPath("//ul[@id='do-productLines']/li/div/div/p/b")).Click();
+            driver.FindElement(By.LinkText("Product Data")).Click();
+
+            PopUp popup3 = new PopUp(driver);
+            SeleniumExtras.PageObjects.PageFactory.InitElements(driver, popup3);
+            popup3.CloseProdTour();
 
             HomePage homePage = new HomePage(driver);
             SeleniumExtras.PageObjects.PageFactory.InitElements(driver, homePage);
